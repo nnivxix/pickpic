25272f036c512f22a6cac4970b3b8ea8fc568ca1
nnivxix <adhe.hanasa@gmail.com>
2025-03-15 13:58:08 +0700

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,16 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: true },
+  modules: [
+    "@nuxt/icon",
+    "@nuxt/image",
+    "@nuxt/fonts",
+    "@nuxtjs/tailwindcss",
+    "shadcn-nuxt",
+  ],
+  shadcn: {
+    prefix: "",
+    componentDir: "./components/ui",
+  },
+});
---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <h1 class="text-4xl text-red-500">Page: index</h1>
+    <Button>Hallo</Button>
+  </div>
+</template>
+
+<style scoped></style>

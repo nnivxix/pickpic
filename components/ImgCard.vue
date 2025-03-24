4aa355be956c57946dda4511505e9b7fad6a86c7
nnivxix <adhe.hanasa@gmail.com>
2025-03-24 14:48:05 +0700

    feat: update ImgCard component to use smaller image source for improved loading performance

--- components/ImgCard.vue
+++ components/ImgCard.vue
@@ -11,7 +11,7 @@ const { photo } = defineProps<{
             class="absolute inset-0 bg-red-300 bg-opacity-0 rounded-lg group-hover:bg-opacity-50 group-hover:-translate-y-2 z-10 transition-all duration-300 ease-in-out pointer-events-none"
         ></div>
         <img
-            :src="photo.urls.regular"
+            :src="photo.urls.small"
             :alt="photo.alt_description"
             :height="photo.height"
             :width="photo.width"

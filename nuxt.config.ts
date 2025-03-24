1cd7628e1efa39de7fe44d49d0e4ad5a3bb751d1
nnivxix <adhe.hanasa@gmail.com>
2025-03-24 14:11:40 +0700

    chore: nuxt update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,30 +1,30 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2024-11-01",
-  devtools: { enabled: true },
-  modules: [
-    "@nuxt/icon",
-    "@nuxt/image",
-    "@nuxt/fonts",
-    "@nuxtjs/tailwindcss",
-    "shadcn-nuxt",
-    "@vueuse/nuxt",
-  ],
-  runtimeConfig: {
-    public: {
-      unsplash: {
-        accessKey: process.env.NUXT_UNSPLASH_ACCESS_KEY,
-        baseUrl: process.env.NUXT_UNSPLASH_BASE_URL,
-      },
+    compatibilityDate: "2025-03-24",
+    devtools: { enabled: true },
+    modules: [
+        "@nuxt/icon",
+        "@nuxt/image",
+        "@nuxt/fonts",
+        "@nuxtjs/tailwindcss",
+        "shadcn-nuxt",
+        "@vueuse/nuxt",
+    ],
+    runtimeConfig: {
+        public: {
+            unsplash: {
+                accessKey: process.env.NUXT_UNSPLASH_ACCESS_KEY,
+                baseUrl: process.env.NUXT_UNSPLASH_BASE_URL,
+            },
+        },
     },
-  },
-  shadcn: {
-    prefix: "",
-    componentDir: "./components/ui",
-  },
+    shadcn: {
+        prefix: "",
+        componentDir: "./components/ui",
+    },
 
-  experimental: {
-    asyncContext: true,
-    typedPages: true,
-  },
+    experimental: {
+        asyncContext: true,
+        typedPages: true,
+    },
 });

80c4323ece31114ed399c280df9c084874279add
nnivxix <adhe.hanasa@gmail.com>
2025-03-24 14:13:04 +0700

    feat: migrate utility functions to a dedicated module and update component imports

--- components/ui/card/CardDescription.vue
+++ components/ui/card/CardDescription.vue
@@ -1,14 +1,13 @@
 <script setup lang="ts">
-import type { HTMLAttributes } from 'vue'
-import { cn } from '@/lib/utils'
+import type { HTMLAttributes } from "vue";
 
 const props = defineProps<{
-  class?: HTMLAttributes['class']
-}>()
+    class?: HTMLAttributes["class"];
+}>();
 </script>
 
 <template>
-  <p :class="cn('text-sm text-muted-foreground', props.class)">
-    <slot />
-  </p>
+    <p :class="cn('text-sm text-muted-foreground', props.class)">
+        <slot />
+    </p>
 </template>

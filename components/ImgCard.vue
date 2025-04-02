2d7d5bc62966dcf8e925c9c5333a5e3c4d532b26
nnivxix <adhe.hanasa@gmail.com>
2025-04-02 16:33:31 +0700

    refactor: ImgCard and MasonryGrid components for improved layout and responsiveness

--- components/ImgCard.vue
+++ components/ImgCard.vue
@@ -4,18 +4,30 @@ import type { Photo } from "~/types/photo";
 const { photo } = defineProps<{
     photo: Photo;
 }>();
+
+const span = computed(() => Math.ceil(photo.height / photo.width));
 </script>
 <template>
-    <NuxtLink :to="`/photos/${photo.id}`" class="py-2 group relative">
-        <div
+    <NuxtLink
+        :data-span="span"
+        :to="`/photos/${photo.id}`"
+        class="py-2 group relative"
+        :style="{ 'grid-row-end': `span ${span}` }"
+    >
+        <!-- <div
+            class="absolute top-2 z-50 left-2 w-8 h-8 bg-gray-600 text-white flex items-center justify-center rounded-full"
+        >
+            {{ index }}
+        </div> -->
+        <!-- <div
             class="absolute inset-0 bg-red-300 bg-opacity-0 rounded-lg group-hover:bg-opacity-50 group-hover:-translate-y-2 z-10 transition-all duration-300 ease-in-out pointer-events-none"
-        ></div>
+        ></div> -->
         <img
             :src="photo.urls.small"
             :alt="photo.alt_description"
             :height="photo.height"
             :width="photo.width"
-            class="object-cover py-1 w-full rounded-lg hover:scale-95 transition-transform duration-300 ease-in-out"
+            class="object-cover py-1 w-full h-full rounded-lg hover:scale-95 transition-transform duration-300 ease-in-out"
         />
     </NuxtLink>
 </template>
--- components/MasonryGrid.vue
+++ components/MasonryGrid.vue
@@ -1,7 +1,9 @@
 <template>
-  <section class="py-5">
-    <div class="columns-2 md:columns-4 gap-2 lg:columns-6 px-4 object-contain">
-      <slot />
-    </div>
-  </section>
+    <section class="py-5">
+        <div
+            class="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-x-4 px-4 object-contain"
+        >
+            <slot />
+        </div>
+    </section>
 </template>

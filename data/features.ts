835458cbcfbce415b41daeb28991c026944ea632
nnivxix <adhe.hanasa@gmail.com>
2025-04-08 16:39:17 +0700

    feat: refactor About page to use external features data for improved maintainability

---
+++ data/features.ts
@@ -0,0 +1,24 @@
+export const features = [
+    {
+        title: "Explore",
+        description:
+            "Discover stunning images from Unsplash's vast library. Dive into high-quality photography, perfect for any project. Our curated selection ensures you'll find the ideal visual to enhance your creative endeavors.",
+        src: "https://images.unsplash.com/photo-1496950866446-3253e1470e8e?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wzMDY1MjF8MHwxfGFsbHx8fHx8fHx8fDE3NDQwMTc4NTN8&ixlib=rb-4.0.3&q=80&w=400",
+        alt: "The Plan before the Adventure",
+    },
+    {
+        title: "Search",
+        description:
+            "Find the perfect image for your project. Our search functionality allows you to quickly locate the visuals you need. With an extensive database of high-resolution photos, you're sure to discover the ideal match for your creative vision.",
+
+        src: "https://images.unsplash.com/photo-1508921340878-ba53e1f016ec?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wzMDY1MjF8MHwxfGFsbHx8fHx8fHx8fDE3NDQwMTc5Mzd8&ixlib=rb-4.0.3&q=80&w=400",
+        alt: "Using Web on iPad",
+    },
+    {
+        title: "Markdown",
+        description:
+            "Copy and paste markdown syntax for easy integration. Streamline your workflow by effortlessly embedding images with the correct attribution. Focus on your content, not the formatting.",
+        src: "https://images.unsplash.com/photo-1611079830811-865ff4428d17?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wzMDY1MjF8MHwxfGFsbHx8fHx8fHx8fDE3NDQwMTc5ODl8&ixlib=rb-4.0.3&q=80&w=400",
+        alt: "A school, office notebook with a white pen lies on a gray table, desk, background. Place for an inscription. Office. Job. School. Personal diary. Top view.",
+    },
+];
--- pages/about.vue
+++ pages/about.vue
@@ -1,37 +1,11 @@
 <script setup lang="ts">
 import { buttonVariants } from "~/components/ui/button";
 import { featuredPhotos as photos } from "~/data/featured-photos";
+import { features } from "~/data/features";
 
 useSeoMeta({
     title: "PickPic - About",
 });
-
-const imageData = {
-    images: [
-        {
-            title: "Explore",
-            description:
-                "Discover stunning images from Unsplash's vast library. Dive into high-quality photography, perfect for any project. Our curated selection ensures you'll find the ideal visual to enhance your creative endeavors.",
-            src: "https://images.unsplash.com/photo-1496950866446-3253e1470e8e?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wzMDY1MjF8MHwxfGFsbHx8fHx8fHx8fDE3NDQwMTc4NTN8&ixlib=rb-4.0.3&q=80&w=400",
-            alt: "The Plan before the Adventure",
-        },
-        {
-            title: "Search",
-            description:
-                "Find the perfect image for your project. Our search functionality allows you to quickly locate the visuals you need. With an extensive database of high-resolution photos, you're sure to discover the ideal match for your creative vision.",
-
-            src: "https://images.unsplash.com/photo-1508921340878-ba53e1f016ec?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wzMDY1MjF8MHwxfGFsbHx8fHx8fHx8fDE3NDQwMTc5Mzd8&ixlib=rb-4.0.3&q=80&w=400",
-            alt: "Using Web on iPad",
-        },
-        {
-            title: "Markdown",
-            description:
-                "Copy and paste markdown syntax for easy integration. Streamline your workflow by effortlessly embedding images with the correct attribution. Focus on your content, not the formatting.",
-            src: "https://images.unsplash.com/photo-1611079830811-865ff4428d17?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wzMDY1MjF8MHwxfGFsbHx8fHx8fHx8fDE3NDQwMTc5ODl8&ixlib=rb-4.0.3&q=80&w=400",
-            alt: "A school, office notebook with a white pen lies on a gray table, desk, background. Place for an inscription. Office. Job. School. Personal diary. Top view.",
-        },
-    ],
-};
 </script>
 
 <template>
@@ -75,17 +49,17 @@ const imageData = {
         <h2 class="text-2xl font-bold text-center">Features</h2>
         <ul class="grid grid-cols-3 gap-6">
             <li
-                v-for="(image, index) in imageData.images"
+                v-for="(feature, index) in features"
                 :key="index"
                 class="flex flex-col gap-2 mb-4 col-span-3 md:col-span-1"
             >
                 <img
-                    :src="image.src"
-                    :alt="image.alt"
+                    :src="feature.src"
+                    :alt="feature.alt"
                     class="aspect-video object-cover rounded-lg"
                 />
-                <h3 class="text-lg font-semibold">{{ image.title }}</h3>
-                <p class="text-muted-foreground">{{ image.description }}</p>
+                <h3 class="text-lg font-semibold">{{ feature.title }}</h3>
+                <p class="text-muted-foreground">{{ feature.description }}</p>
             </li>
         </ul>
     </section>

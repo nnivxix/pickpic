e5b49c1623f07edbf9dbb88dce6dd4af9a6fbbbc
nnivxix <adhe.hanasa@gmail.com>
2025-04-13 23:08:46 +0700

    refactor: remove unused code and comments from default middleware

--- server/middleware/default.ts
+++ server/middleware/default.ts
@@ -1,9 +1,1 @@
-export default defineEventHandler((event) => {
-    const url = getRequestURL(event);
-    const runtime = useRuntimeConfig();
-    // console.log("appUrl =>", runtime.public.appUrl);
-
-    if (url.pathname === "/api/photos") {
-        // console.log("default middleware");
-    }
-});
+export default defineEventHandler((event) => {});

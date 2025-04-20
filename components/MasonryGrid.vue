268fe40b7d9b81b7706eb3d2965d70b5574b211b
nnivxix <adhe.hanasa@gmail.com>
2025-04-20 16:15:59 +0700

    refactor: remove unused components and API endpoints

--- components/MasonryGrid.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <section class="py-5">
-        <div
-            class="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-x-4 px-4 object-contain"
-        >
-            <slot />
-        </div>
-    </section>
-</template>
